b446e55895c88b490aaee48c396417685f52e7df
silvertechsavvy <silvertechsavvy@gmail.com>
2021-01-22 16:56:11 +0200

    feat: add base tailwind styles, add routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+import './styles/index.css';
+import { App } from './components';
+
+const Entry = () => (
+  <Router>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </Router>
 );
+
+ReactDOM.render(<Entry />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
