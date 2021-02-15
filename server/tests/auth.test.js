331da5701b9fe46f84b61b36ff1748bf4b8b8f5e
silvertechsavvy <silvertechsavvy@gmail.com>
2021-02-15 16:32:42 +0200

    tests: add tests for signup post api

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -1,16 +1,18 @@
 const request = require('supertest');
 const app = require('../app');
 const db = require('./db');
+const User = require('../models/User');
 
-// Setup connection to the database
-beforeAll(async () => await db.connect());
-// afterEach(async () => await db.clear());
-afterAll(async () => await db.close());
-
+// A valid user input
 const USER_REQUEST = {
   email: 'example@test.com',
   password: 'password1234',
 };
+
+// Setup connection to the database
+beforeAll(async () => await db.connect());
+beforeEach(async () => await db.clear());
+afterAll(async () => await db.close());
 
 // Login tests
 describe('POST /api/user/login', () => {
@@ -26,8 +28,10 @@ describe('POST /api/user/login', () => {
 
 // Sign Up tests
 describe('POST /api/user/signup', () => {
+  const agent = request.agent(app);
+
   test('It should response with 201 status', done => {
-    request(app)
+    agent
       .post('/api/user/signup')
       .send(USER_REQUEST)
       .then(res => {
@@ -37,11 +41,56 @@ describe('POST /api/user/signup', () => {
   });
 
   test('It should store user to db', done => {
-    request(app)
+    agent
       .post('/api/user/signup')
       .send(USER_REQUEST)
+      .expect(201)
       .then(res => {
-        expect(JSON.parse(res.text)).toHaveProperty(['_id']);
+        expect(res.body.user).toBeTruthy();
+        done();
+      });
+  });
+
+  test('It should throw not unique email error', async done => {
+    // Create user a new user in db
+    await User.create(USER_REQUEST);
+
+    // Try to sign up using the same email
+    await agent
+      .post('/api/user/signup')
+      .send(USER_REQUEST)
+      .expect(400)
+      .then(res => {
+        expect(res.body.errors.email).toBeTruthy();
+        done();
+      });
+  });
+
+  test('It should throw invalid email error', done => {
+    agent
+      .post('/api/user/signup')
+      .send({
+        email: 'test@',
+        password: 'password1234',
+      })
+      .expect(400)
+      .then(res => {
+        expect(res.body.errors.email).toBeTruthy();
+        done();
+      });
+  });
+
+  test('It should throw invalid input length error', done => {
+    agent
+      .post('/api/user/signup')
+      .send({
+        email: 'w',
+        password: 'w',
+      })
+      .expect(400)
+      .then(res => {
+        expect(res.body.errors.email).toBeTruthy();
+        expect(res.body.errors.password).toBeTruthy();
         done();
       });
   });
