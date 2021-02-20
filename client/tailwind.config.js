2c9fea6bd051340c7ede0b22223f8624195dfaa0
silvertechsavvy <silvertechsavvy@gmail.com>
2021-02-20 21:29:17 +0200

    chore: add router and update tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        almostBlack: '#1C1B21',
+        slate: {
+          100: '#FBFBFC',
+          200: '#C3C3C9',
+          300: '#51525C',
+          400: '#292930',
+        },
+        salmon: '#FA8072',
+      },
+      fontFamily: {
+        primary: ['Quicksand'],
+      },
+    },
   },
   variants: {},
   plugins: [],
