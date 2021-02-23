c955e5f72dee105a935b21785a111bd572c477b9
silvertechsavvy <silvertechsavvy@gmail.com>
2021-02-23 17:30:51 +0200

    feat: add icons and logo

---
+++ client/src/components/icons/index.js
@@ -0,0 +1,17 @@
+import { default as BrandingIcon } from './Branding';
+import { default as GithubIcon } from './Github';
+import { default as MapIcon } from './Map';
+import { default as MenuIcon } from './Menu';
+import { default as SettingsIcon } from './Settings';
+import { default as AddIcon } from './Add';
+import { default as LogoIcon } from './Logo';
+
+export {
+  BrandingIcon,
+  GithubIcon,
+  MapIcon,
+  MenuIcon,
+  SettingsIcon,
+  AddIcon,
+  LogoIcon,
+};
